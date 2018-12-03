879b809b86cfa36c28a7094e41c84bc88bd86e50
Meena.Hoda <meena.hoda@wmfs.net>
2018-12-03 10:00:24 +0000

    refactor: bit more implementation

--- test/graphql-spec.js
+++ test/graphql-spec.js
@@ -1,8 +1,20 @@
 /* eslint-env mocha */
 
+const PORT = 3003
+const URL = `http://localhost:${PORT}/graphql`
+
 const path = require('path')
 const tymly = require('@wmfs/tymly')
 const expect = require('chai').expect
+const jwt = require('jsonwebtoken')
+const request = require('request')
+const Buffer = require('safe-buffer').Buffer
+
+function sendToken (adminToken) {
+  const options = { Accept: '*/*' }
+  if (adminToken) options.authorization = 'Bearer ' + adminToken
+  return options
+}
 
 describe('GraphQL tests', function () {
   this.timeout(process.env.TIMEOUT || 5000)
@@ -10,7 +22,11 @@ describe('GraphQL tests', function () {
   const secret = 'Shhh!'
   const audience = 'IAmTheAudience!'
 
-  let tymlyService
+  let tymlyService, adminToken, server
+
+  it('create a usable admin token for Dave', () => {
+    adminToken = jwt.sign({}, Buffer.from(secret, 'base64'), { subject: 'Dave', audience })
+  })
 
   it('boot Tymly', done => {
     tymly.boot(
@@ -25,13 +41,38 @@ describe('GraphQL tests', function () {
           path.resolve(__dirname, './../test/fixtures/pizza-blueprint')
         ],
         config: {
-          auth: { secret, audience }
+          auth: { secret, audience },
+          defaultUsers: { Dave: 'tymlyTest_tymlyTestAdmin' }
         }
       },
       (err, services) => {
         expect(err).to.eql(null)
         tymlyService = services.tymly
+        server = services.server
         done()
+      }
+    )
+  })
+
+  it('start GraphQL app', done => {
+    server.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}.`)
+      done()
+    })
+  })
+
+  it('attempt to get templates', done => {
+    request(
+      {
+        url: URL,
+        method: 'POST',
+        json: {
+          query: '{templates}'
+        }
+      },
+      (err, res, body) => {
+        expect(res.statusCode).to.equal(200)
+        done(err)
       }
     )
   })
